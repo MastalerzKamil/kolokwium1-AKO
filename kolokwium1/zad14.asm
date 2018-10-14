58023cf438f5e07ebdb82f9578c0b7c7e63d29c2
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2018-10-14 22:44:40 +0200

    Correct zad14

--- kolokwium1/zad14.asm
+++ kolokwium1/zad14.asm
@@ -1,14 +1,20 @@
 .686
 .model flat
 extern _ExitProcess@4 : PROC
-;public _main
+public _main
 .data
 
 .code
-;_main PROC
+_main PROC
+	mov eax, 1
+	mov ebx, 2
+	push eax
+	push ebx
+	add esp, 8
+
 	sub esp,8
-	mov [esp+8], eax
+	mov [esp+4], eax
 	mov [esp], ebx
 	call _ExitProcess@4
-;_main ENDP
+_main ENDP
 END
--- kolokwium1/zad15.asm
+++ kolokwium1/zad15.asm
@@ -1,14 +1,14 @@
 .686
 .model flat
 extern _ExitProcess@4 : PROC
-public _main
+;public _main
 .data
 
 .code
-_main PROC
+;_main PROC
 	mov eax, 0ffffffffh
 	mov ebx, 0ffffffffh
 	imul ebx
 	call _ExitProcess@4
-_main ENDP
+;_main ENDP
 END
