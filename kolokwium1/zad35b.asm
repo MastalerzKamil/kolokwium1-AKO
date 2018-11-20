870ea6997bcd6a68cd047e19b57287c7e744f925
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2018-11-20 23:19:22 +0100

    Add spliting digits for calendar month in ex 35b

--- kolokwium1/zad35b.asm
+++ kolokwium1/zad35b.asm
@@ -6,11 +6,33 @@ public _main
 
 .data
 ilosc_swiat		dd 0
+swieta			dd 0AAAAAAAAh
 wynik			dd ?,'.',?,0,'\n'
+miesiac			dd 0, 0
+dzien			dd 0, 0
+system_liczbowy db 10
 .code
 _main PROC
 	; edx - iterator po bitach miesiaca
 	; esi - element tablicy dni miesiaca
+	mov ecx, 9
+	shl ecx, 24
+	shr ecx, 24	; aby miec pewnosc, ze starsze bity w ecx poza cl sa zerem
+	mov ax, cx	; aby w dzielnej(liczniku dzielenia) byla wartosc miesiaca
+	push ecx	; zapamietuje ecx aby rejestr ten posluzyl do kelejnon zapisywania w pamieci
+	push edi	; zapamietanie celu do ktorego trzeba zapisac. W tym przypadku celem bedzie zmienna miesiac
+	mov ecx, 2	; maksymalna ilosc cyfer w miesiacu
+	mov edi, offset miesiac	; miejsce do ktorego bede zapisywal wynik zdekodowanego miesiaca
+licz_miesiac_dziesietnie:
+	div system_liczbowy
+	dec ecx
+	mov byte PTR[edi+4*ecx], ah	; wyliczanie adresu efektywnego dla miesiaca w pamieci czyli adres+4*ilosc_cyfer
+	cmp al, 0
+	jnz licz_miesiac_dziesietnie
+	pop edi	; zdjecie sprzed zapisu miesiaca 
+	pop ecx	; zdjecie zawartosci miesiaca w ecx
+
+	mov ebx, offset swieta ; testowo dalem zmienna ze swietami
 	mov esi, [ebx]
 	mov edx, 31	; najstarszy bit do testowania
 ptl:
@@ -20,17 +42,16 @@ ptl:
 	jnz ptl	;  przyadek gdy nie ma swieta  ale tez nie ma konca miesiaca
 	jmp koniec_miesiaca ; przypadek gdy jest koniec miesiaca i nie bylo swieta
 jest_swieto:
-	shl ecx, 24
-	shr ecx, 24	; aby miec pewnosc, ze starsze bity w ecx poza cl sa zerem
 	add ilosc_swiat, 1
-	push dword PTR 4+4+4+4 ; push(dzien+'.'+miesiac+0)
+	push dword PTR 4+4+4+4 ; push(dzien+dzien+'.'+miesiac+miesiac+0)
+	add edx, '0'
 	mov wynik, edx	; pierwszy czlon daty
 	mov edi,(offset wynik)+8 ; adres pod ktorym bedzie miesiac
 	mov ecx, [edi] ; wartosc miesiaca  format: edx.ecx
 	push OFFSET wynik	; wartosc daty
 	push dword PTR 1	; numer urzadzenia (numer ekranu=1)
 	call __write
-	; add esp, 12	; zwolnienie pamieci po wypisaniu daty
+	add esp, 12	; zwolnienie pamieci po wypisaniu daty
 	dec edx ; aby kolejnybit wziac pod uwage
 	jnz ptl	; jeszcze nie ma konca miesiaca
 	jmp koniec_miesiaca	
